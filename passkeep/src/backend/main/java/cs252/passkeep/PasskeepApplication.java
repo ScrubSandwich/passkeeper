9444280ec9ea5f31390a7c396618c55c55070edf
hmontoyag <35666487+hmontoyag@users.noreply.github.com>
2018-04-22 17:00:37 -0400

    Added new endpoints

--- passkeep/src/backend/main/java/cs252/passkeep/PasskeepApplication.java
+++ passkeep/src/backend/main/java/cs252/passkeep/PasskeepApplication.java
@@ -1,5 +1,7 @@
 package cs252.passkeep;
 
+import cs252.passkeep.api.ContentController;
+import cs252.passkeep.api.FeaturesController;
 import cs252.passkeep.api.LoginController;
 import cs252.passkeep.api.SignupController;
 import org.springframework.boot.SpringApplication;
@@ -24,6 +26,12 @@ public class PasskeepApplication {
 	@Autowired
     private LoginController loginController;
 
+	@Autowired
+	private ContentController contentController;
+
+	@Autowired
+	private FeaturesController featuresController;
+
 
 	@CrossOrigin
 	@RequestMapping(value = "/sign-up", method = RequestMethod.POST)
@@ -40,7 +48,43 @@ public class PasskeepApplication {
         return loginController.login(body);
 
     }
+	@CrossOrigin
+	@RequestMapping(value = "/logout", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> logout(@Valid @RequestBody Map<String, Object> body) {
+		return loginController.logout(body);
 
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = "/record/add", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> addRecord(@Valid @RequestBody Map<String, Object> body) {
+		return contentController.addRecord(body);
+
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = "/record/get", params = {"id"}, method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String, Object> getProfile(@RequestParam String id,@RequestParam String token, @RequestParam String query){
+		return contentController.getRecord(id,token,query);
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = "/record/list", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> getList(@Valid @RequestBody Map<String, Object> body) {
+		return contentController.getList(body);
+
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = "/generate", params = {"id"}, method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String, Object> generateRandom(@RequestParam String id,@RequestParam String token,@RequestParam int len, @RequestParam String query){
+		return featuresController.generateRandom(id,token,len,query);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PasskeepApplication.class, args);
 	}
