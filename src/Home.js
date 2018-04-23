d3372e06ba27fb85885d18e1e3db8d5127f11cfe
jmieczni <jmieczni@purdue.edu>
2018-04-23 17:59:18 -0400

    Add Empty Modal for create new record button

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Redirect, Switch} from 'react-router-dom'
+import { Alert } from 'react-bootstrap'
 import Nav from './Navigation'
 import "./Home.css"
 
@@ -8,7 +9,7 @@ class Home extends Component {
     super(props, context);
 
     this.state = {
-      records: [{"title": "Gmail Throwaway 1", "id": 1}, {"title": "yahoo", "id": 2}],
+      records: [],
     }
   }
 
@@ -52,7 +53,8 @@ class Home extends Component {
       return (
         <h3>No records yet! Click the "New Record" button above to create a record.</h3>
       )
-    }
+    } 
+    
     return (
       <div>
         <h1>Records</h1>
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Redirect, Switch} from 'react-router-dom'
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Modal, OverlayTrigger, Button } from 'react-bootstrap'
 
 class Navigation extends Component {
-  constructor(props) {
-    super(props);
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
 
     this.state = {
       name: localStorage.getItem('name'),
+      show: false,
     }
   }
-  
-  render() {
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  renderNav = () => {
     const name = "Hello, " + this.state.name;
+
     return (
       <Navbar inverse collapseOnSelect>
         <Navbar.Header>
@@ -21,7 +34,7 @@ class Navigation extends Component {
           </Navbar.Brand>
         </Navbar.Header>
         <Nav pullRight>
-          <NavItem eventKey={1} href="/new-record">
+          <NavItem eventKey={1} onClick={this.handleShow} >
             New Record
           </NavItem>
           <NavDropdown eventKey={2} title={name} id="basic-nav-dropdown">
@@ -33,6 +46,35 @@ class Navigation extends Component {
           </NavDropdown>
         </Nav>
       </Navbar>
+    )
+  }
+
+  handleSubmit = (e) => {
+    console.log('sd')
+  }
+
+  renderModal = () => {
+    return (
+      <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title> Create New Record</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleSubmit}>Submit</Button>
+          </Modal.Footer>
+        </Modal>
+    )
+  }
+  
+  render() {
+    return (
+      <div>
+        {this.renderNav()}
+        {this.renderModal()}
+      </div>  
     )
   }
 }
