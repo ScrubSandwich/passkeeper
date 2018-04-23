73d872fdf387a7ce6c5c56f77ac6c09006381982
jmieczni <jmieczni@purdue.edu>
2018-04-23 01:11:00 -0400

    Hook up home with the backend

--- src/Home.js
+++ src/Home.js
@@ -12,7 +12,47 @@ class Home extends Component {
     }
   }
 
+  getLocalStorage = (key) => {
+    return localStorage.getItem(key);
+  }
+
+  componentWillMount = () => {
+    const _this = this;
+
+    let payload = JSON.stringify({
+      "userId": this.getLocalStorage("id"),
+      "token": this.getLocalStorage("token"),
+    });
+
+    fetch("http://localhost:8080/record/list", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+        'transfer-encoding': 'chunked',
+      },
+      body: payload,
+    })
+    .then(function(response) {
+      response.json().then(json => {
+        console.log(json)
+        if (json.status == "OK") {
+          _this.setState({
+            records: json.list,
+          })
+        } else {
+          alert("Unable to connect to the server.")
+        }
+      });
+    })
+  }
+
   renderRecords = () => {
+    if (this.state.records.length == 0) {
+      return (
+        <h3>No records yet! Click the "New Record" button above to create a record.</h3>
+      )
+    }
     return (
       <div>
         <h1>Records</h1>
--- src/Login.js
+++ src/Login.js
@@ -85,7 +85,6 @@ class Login extends Component {
         }
       });
     })
-
   }
 
   renderLoginForm = () => {
