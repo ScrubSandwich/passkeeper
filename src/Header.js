dc5826dab7bd019e1c4eb2e38371648ec222f5e3
jmieczni <jmieczni@purdue.edu>
2018-04-24 18:28:36 -0400

    Add link to logo to return to /

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect, Switch} from 'react-router-dom'
 import './Welcome.css'
 
 class Header extends Component {
   render() {
     return (
       <div className="App-header">
-        <div className="title">
-            <strong className="trash">pass</strong>
-            <strong className="pass">keep</strong>
-        </div>
+        <Link to="/">
+          <div className="title">
+              <strong className="trash">pass</strong>
+              <strong className="pass">keep</strong>
+          </div>
+        </Link>
         <p className="intro">
           A simple throw-away account information manager.
         </p>
