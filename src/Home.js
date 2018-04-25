6b046376665ae913477669ac9dd8e854185958ce
jmieczni <jmieczni@purdue.edu>
2018-04-24 22:51:16 -0400

    Add delete button to home

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Redirect, Switch} from 'react-router-dom'
-import { Table, Breadcrumb } from 'react-bootstrap'
+import { Table, Breadcrumb, Button } from 'react-bootstrap'
 import Nav from './Navigation'
 import "./Home.css"
 
@@ -10,11 +10,12 @@ class Home extends Component {
 
     this.state = {
       records: [],
+      delete: false,
     }
   }
 
   componentDidUpdate = () => {
-    console.log(this.state)
+    //console.log(this.state)
   }
 
   getLocalStorage = (key) => {
@@ -51,13 +52,72 @@ class Home extends Component {
     })
   }
 
+  handleDelete = (id) => {    
+    const _this = this;
+    const idUser = localStorage.getItem("id");
+    const token = localStorage.getItem("token");
+
+    let payload = JSON.stringify({
+      "userId": idUser,
+      "token": token,
+      "recordId": id,
+    });
+
+    fetch("https://passkeep.herokuapp.com/record/delete", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+        'transfer-encoding': 'chunked',
+      },
+      body: payload,
+    })
+    .then(function(response) {
+      response.json().then(json => {
+        if (json.status == "OK") {
+          console.log(json)
+          alert("Record Deleted")
+          window.location.reload();
+        } else {
+          alert(json.message)
+        }
+      });
+    })
+  }
+
+  renderButton = (id) => {
+    if (this.state.delete) {
+      return (
+        <div className="button">
+          <div className="delete">
+            <Button bsStyle="danger" onClick={(e) =>{
+              {this.handleDelete(id)}
+            }}>
+              Delete Record
+            </Button>
+          </div>
+        </div>
+      )
+    }    
+  }
+
   renderRecords = () => {
     if (this.state.records.length == 0) {
       return (
         <h3>No records yet! Click the "New Record" button above to create a record.</h3>
       )
-    } 
-    
+    }
+
+    var recordStyle = {};
+
+    //If delete button is hit, then use these style
+    if (this.state.delete) {
+      recordStyle = {
+        display: 'grid',
+        gridTemplateColumns: '300px 50px',
+      }
+    }
+
     return (
       <div>
         <h1>All Records</h1>
@@ -71,15 +131,16 @@ class Home extends Component {
             </thead>
             <tbody>              
                 {this.state.records.map((record, index) =>
-                  <tr>
+                  <tr key={index}>
                     <td>
                       <h2>{index + 1}</h2>
                     </td>
                     <td>
-                      <div className="record" key={index}>
+                      <div className="record" key={index} style={recordStyle}>
                         <Link to={"/view-record?id=" + record.record_id}>
                           <h2>{record.title}</h2>
                         </Link>
+                        {this.renderButton(record.record_id)}
                       </div>
                     </td>
                   </tr>
@@ -101,11 +162,27 @@ class Home extends Component {
     )
   }
 
+  renderDeleteButton = () => {
+    return (
+      <div className="deleteButton">
+        <Button onClick={this.toggleDelete} bsStyle="warning">Delete A Record</Button>
+      </div>
+    )
+  }
+
+  toggleDelete = () => {
+    const s = !this.state.delete;
+    this.setState({ delete: s, })
+  }
+
   render() {
     return (
       <div className="home">
         <Nav />
-        {this.renderBread()}
+        <div className="top">
+          {this.renderBread()}
+          {this.renderDeleteButton()}
+        </div>
         {this.renderRecords()}
       </div>
     )
